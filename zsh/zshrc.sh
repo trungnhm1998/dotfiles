3125aca3a6dbd418b83fd675ae858d355b284d7b
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2025-01-26 22:46:49 +0700

    feat: yazi function

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -22,6 +22,15 @@ export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
+
 ZSH_THEME="spaceship"
 ZSH_DISABLE_COMPFIX=true
 
