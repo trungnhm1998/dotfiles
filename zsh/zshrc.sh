bfe735a4be0a4d1cb923e4df5f4507330c03f077
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2025-01-27 14:28:41 +0700

    feat: config y, zoxide, eza

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,15 +1,10 @@
-export ZSH=$HOME/.oh-my-zsh
-export ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
 export PATH=$HOME/bin:usr/local/bin:$PATH
 export PATH=$HOME/.local/share/umake/bin:$PATH
 export PATH=$PATH:/usr/local/nodejs/bin
-export ANDROID_HOME=/Users/iosteam/Library/Android/sdk
-export PATH=$PATH:$ANDROID_HOME
-export PATH=$PATH:$ANDROID_HOME/platform-tools
-export PATH=$PATH:$ANDROID_HOME/emulator
-export TERM=xterm-256color
-export VISUAL=vim
+export VISUAL=nvim
 export EDITOR="$VISUAL"
+
+# --- nvim ---
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 # install https://github.com/sharkdp/vivid/releases for using vivid below
@@ -18,26 +13,34 @@ if [ -x "$(command -v vivid)" ]; then
   export LS_COLORS=$LS_COLORS:"tw=30;42:ow=30;42"
 fi
 
+# --- pyenv ---
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 
+# --- ruby env ---
 export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+if which rbenv >/dev/null; then eval "$(rbenv init -)"; fi
+if which rbenv >/dev/null; then eval "$(rbenv init -)"; fi
+if which rbenv >/dev/null; then eval "$(rbenv init -)"; fi
 
 export GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
 export PATH="$PATH:$GEM_HOME/bin"
 
+# --- yazi cd to directory after exit ---
 function y() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-	yazi "$@" --cwd-file="$tmp"
-	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		builtin cd -- "$cwd"
-	fi
-	rm -f -- "$tmp"
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  yazi "$@" --cwd-file="$tmp"
+  if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    builtin cd -- "$cwd"
+  fi
+  rm -f -- "$tmp"
 }
+
+# --- zsh and oh-my-zsh ---
+
+export ZSH=$HOME/.oh-my-zsh
+export ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
 
 ZSH_THEME="spaceship"
 ZSH_DISABLE_COMPFIX=true
@@ -54,7 +57,45 @@ plugins+=(
 
 #alias tmux="TERM=screen-256color-bce tmux"
 
-bindkey -v # YAY VIM MODE
-
 source $ZSH/oh-my-zsh.sh
+
+# bindkey -v # YAY VIM MODE
+
 source ~/dotfiles/zsh/keybindings.sh
+
+# ---- FZF ----
+# Set up fzf key bindings and fuzzy completion
+source <(fzf --zsh)
+
+# -- Use fs instead of fzf --
+export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+
+_fzf_compgen_path() {
+  fd --hidden --exclude .git . "$1"
+}
+
+_fzf_compgen_dir() {
+  fd --type=d --hidden --exclude .git . "$1"
+}
+
+source ~/fzf-git.sh/fzf-git.sh
+
+# --- zoxide ---
+eval "$(zoxide init zsh)"
+alias cd='z'
+
+# --- eza ---
+alias ls="eza --icons"
+alias l="eza --icons"
+alias ll="eza -lg --icons"
+alias la="eza -lag --icons"
+alias lt="eza -lTg --icons"
+alias lt1="eza -lTg --level=1 --icons"
+alias lt2="eza -lTg --level=2 --icons"
+alias lt3="eza -lTg --level=3 --icons"
+alias lta="eza -lTag --icons"
+alias lta1="eza -lTag --level=1 --icons"
+alias lta2="eza -lTag --level=2 --icons"
+alias lta3="eza -lTag --level=3 --icons"
