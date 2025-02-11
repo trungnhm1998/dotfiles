f6cbaae5d7aabd0255a26ab068684575cf1d7dbf
Nguyen Hoang Minh Trung <trung.nguyenhoangminh1998@gmail.com>
2025-02-11 13:04:00 +0700

    feat(nvim): always show dotfiles

---
+++ .config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          always_show_by_pattern = {
+            "..*",
+          },
+        },
+      },
+    },
+  },
+}
