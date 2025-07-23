3b6c367dec506f92f3ace94f00d6de3f3ac8149b
Nguyen Hoang Minh Trung <trung.nguyen@sun.studio>
2025-07-23 09:33:56 +0700

    fix(idea): rider unable to load due to time_out
    using if statement here to skip the timeout delay for rider or idea

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,13 +1,15 @@
-time_out() { perl -e 'alarm shift; exec @ARGV' "$@"; }
+if [ -z "$INTELLIJ_ENVIRONMENT_READER" ]; then
+  time_out() { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
-# Run tmux if exists
-if command -v tmux >/dev/null; then
-  [ -z $TMUX ] && exec tmux
-else
-  echo "tmux not installed. Run ./deploy to configure dependencies"
+  # Run tmux if exists
+  if command -v tmux >/dev/null; then
+    [ -z $TMUX ] && exec tmux
+  else
+    echo "tmux not installed. Run ./deploy to configure dependencies"
+  fi
+
+  echo "Updating configuration"
 fi
-
-echo "Updating configuration"
 # (cd ~/dotfiles && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
 # (cd ~/dotfiles && git pull && git submodule update --init --recursive)
 source ~/dotfiles/zsh/zshrc.sh
