0c08369b66c052b339f8fb1beeccf1c6091492ec
Nguyen Hoang Minh Trung <trung.nguyen@sun.studio>
2025-07-26 16:28:13 +0700

    refactor(macos): os specific setup script

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,10 +1,14 @@
+#!/bin/zsh
+xcode-select --install
 # install homebrew first
 # maybe check for homebrew exist?
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# command lines tools
+# taps
 brew tap FelixKratz/formulae
+brew tap koekeishiya/formulae
 
+# command lines tools
 brew install \
   neovim \
   node \
@@ -34,7 +38,13 @@ brew install \
   gawk \
   pyenv \
   tldr \
-  sketchybar
+  sketchybar \
+  zoxide \
+  gh \
+  skhd \
+  borders \
+  yabai \
+
 
 # HomeBrew casks
 brew install --cask \
@@ -49,5 +59,19 @@ brew install --cask \
   sketchybar \
   font-sketchybar-app-font \
   sf-symbols # for sketchybar
+  font-sf-mono \
+  font-sf-pro \
+  font-hack-nerd-font \
+  font-jetbrains-mono \
+  font-fira-code
+
+ln -sf $HOME/dotfiles/.config/yabai $HOME/.config/yabai
+ln -sf $HOME/dotfiles/.config/skhd $HOME/.config/skhd
+ln -sf $HOME/dotfiles/.config/jankyborders $HOME/.config/jankyborders
+ln -sf $HOME/dotfiles/.config/sketchybar $HOME/.config/sketchybar
+ln -s $(which sketchybar) $(dirname $(which sketchybar))/external_bar # to use multiple bars
+ln -sf $HOME/dotfiles/.config/external_bar $HOME/.config/external_bar
+ln -sf $HOME/dotfiles/.config/svim $HOME/.config/svim
 
 brew services start sketchybar
+
