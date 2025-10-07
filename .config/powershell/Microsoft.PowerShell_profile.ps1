b302fd4682f23ef3cfda6e7d64b4562cb53b94dc
Nguyen Hoang Minh Trung <trung.nguyenhoangminh1998@gmail.com>
2025-10-07 10:31:25 +0700

    feat(powershell): windows utils

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,27 @@
-Import-Module -Name Terminal-Icons
+$env:_ZO_DATA_DIR = "$HOME\ZoxideData"
+# $poshThemesDir = "$(scoop prefix oh-my-posh)\themes"
+# $randomTheme = "clean-detailed.omp.json", "spaceship.omp.json" | Get-Random
+# oh-my-posh init pwsh --config  $poshThemesDir\$randomTheme | Invoke-Expression
+Import-Module posh-git
+Import-Module Terminal-Icons
 
 Import-Module -Name PSReadLine
 Set-PSReadLineOption -EditMode vi
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
 
-#Import-Module -Name PSColors
-
-Invoke-Expression (&starship init powershell)
 
 # Yazi
 function y {
@@ -23,9 +34,6 @@ function y {
     Remove-Item -Path $tmp
 }
 
-# Alias for zoxide
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
-function cd { z $args }
 
 # --- eza ---
 function ls { eza --icons $args }
@@ -40,4 +48,85 @@ function lta { eza -lTag --icons $args }
 function lta1 { eza -lTag --level=1 --icons $args }
 function lta2 { eza -lTag --level=2 --icons $args }
 function lta3 { eza -lTag --level=3 --icons $args }
+
+Invoke-Expression (&starship init powershell)
+# =============================================================================
+#
+# Utility functions for zoxide.
+#
+
+# Call zoxide binary, returning the output as UTF-8.
+function global:__zoxide_bin {
+    $encoding = [Console]::OutputEncoding
+    try {
+        [Console]::OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $result = zoxide @args
+        return $result
+    } finally {
+        [Console]::OutputEncoding = $encoding
+    }
+}
+
+# pwd based on zoxide's format.
+function global:__zoxide_pwd {
+    $cwd = Get-Location
+    if ($cwd.Provider.Name -eq "FileSystem") {
+        $cwd.ProviderPath
+    }
+}
+
+# cd + custom logic based on the value of _ZO_ECHO.
+function global:__zoxide_cd($dir, $literal) {
+    $dir = if ($literal) {
+        Set-Location -LiteralPath $dir -Passthru -ErrorAction Stop
+    } else {
+        if ($dir -eq '-' -and ($PSVersionTable.PSVersion -lt 6.1)) {
+            Write-Error "cd - is not supported below PowerShell 6.1. Please upgrade your version of PowerShell."
+        }
+        elseif ($dir -eq '+' -and ($PSVersionTable.PSVersion -lt 6.2)) {
+            Write-Error "cd + is not supported below PowerShell 6.2. Please upgrade your version of PowerShell."
+        }
+        else {
+            Set-Location -Path $dir -Passthru -ErrorAction Stop
+        }
+    }
+}
+
+# =============================================================================
+#
+# Hook configuration for zoxide.
+#
+
+# Hook to add new entries to the database.
+$global:__zoxide_oldpwd = __zoxide_pwd
+function global:__zoxide_hook {
+    $result = __zoxide_pwd
+    if ($result -ne $global:__zoxide_oldpwd) {
+        if ($null -ne $result) {
+            zoxide add "--" $result
+        }
+        $global:__zoxide_oldpwd = $result
+    }
+}
+
+# Initialize hook.
+$global:__zoxide_hooked = (Get-Variable __zoxide_hooked -ErrorAction SilentlyContinue -ValueOnly)
+if ($global:__zoxide_hooked -ne 1) {
+    $global:__zoxide_hooked = 1
+    $global:__zoxide_prompt_old = $function:prompt
+
+    function global:prompt {
+        if ($null -ne $__zoxide_prompt_old) {
+            & $__zoxide_prompt_old
+        }
+        $null = __zoxide_hook
+    }
+}
+
+Set-Alias -Name z -Value __zoxide_z -Option AllScope -Scope Global -Force
+Set-Alias -Name zi -Value __zoxide_zi -Option AllScope -Scope Global -Force
+function cd { z $args }
+
+# Alias for zoxide
+Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
 
