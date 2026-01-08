88856c2b2a3fa341a4d5dcb77e70a39228d572ee
Nguyen Hoang Minh Trung <trung.nguyenhoangminh1998@gmail.com>
2026-01-08 18:01:10 +0700

    feat(nvim, window): obsidian plugin

---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,36 @@
+local path_to_workspace = ""
+
+if vim.fn.has("win32") then
+  path_to_workspace = "Z:\\My Drive\\ObsidianVaults\\"
+  -- table.insert(workspaces, {
+  --   name = "vault",
+  --   path = "Z:\\My Drive\\ObsidianVaults",
+  -- })
+end
+
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre " .. path_to_workspace .. "\\*.md",
+  --   "BufNewFile " .. path_to_workspace .. "\\*.md",
+  -- },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "vault",
+        path = path_to_workspace,
+      },
+    },
+  },
+}
