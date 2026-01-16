5660e85fef185061b0cfb7a82cde2bdbd1dbcfc1
Nguyen Hoang Minh Trung <trung.nguyenhoangminh1998@gmail.com>
2026-01-16 21:27:30 +0700

    fix(nvim, obsidian): daily directory and format

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -34,5 +34,17 @@ return {
       },
     },
     ui = { enable = false },
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "10.Daily",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%YY%m%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { "daily-notes" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = nil,
+    },
   },
 }
