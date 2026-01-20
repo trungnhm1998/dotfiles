958d28a014a3f54f9e14757c8443f9c4f3a3b8fa
Nguyen Hoang Minh Trung <trung.nguyenhoangminh1998@gmail.com>
2026-01-20 16:48:18 +0700

    fix: wezterm type

--- .config/nvim/lua/plugins/wezterm-type.lua
+++ .config/nvim/lua/plugins/wezterm-type.lua
@@ -1,17 +1,5 @@
 return {
-  'folke/lazydev.nvim',
-  ft = 'lua',
-  dependencies = {
-    {
-      'DrKJeff16/wezterm-types',
-      lazy = true,
-      version = false, -- Get the latest version
-    },
-  },
-  opts = {
-    library = {
-      -- Other library configs...
-      { path = 'wezterm-types', mods = { 'wezterm' } },
-    },
-  },
+  "DrKJeff16/wezterm-types",
+  lazy = true,
+  version = false, -- Get the latest version
 }
