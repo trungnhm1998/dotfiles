67f36c8712d51a618b5034719bd60c14f7570d31
Nguyen Hoang Minh Trung <trung.nguyenhoangminh1998@gmail.com>
2026-01-21 23:22:40 +0700

    fix(powershell): broken fzf keybind

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,19 +1,21 @@
 $env:_ZO_DATA_DIR = "$HOME\ZoxideData"
-$env:EDITOR = "nvim"
-$env:VISUAL = "nvim"
+# $env:EDITOR = "code --wait"
+# $env:VISUAL = "nvim"
 # $poshThemesDir = "$(scoop prefix oh-my-posh)\themes"
 # $randomTheme = "clean-detailed.omp.json", "spaceship.omp.json" | Get-Random
 # oh-my-posh init pwsh --config  $poshThemesDir\$randomTheme | Invoke-Expression
+
 Import-Module posh-git
 Import-Module Terminal-Icons
-Import-Module -Name PSReadLine
+Import-Module PSReadLine
 Import-Module CompletionPredictor # Install-Module CompletionPredictor -Scope CurrentUser
-Import-Module PSFzf
-
+Import-Module PSFzf # Install-Module -Name PSFzf -Scope CurrentUser -Forcef
+Set-PSReadLineOption -EditMode vi
+# Ovrride vi mode ctrl r
 Set-PsFzfOption -PSReadlineChordProvider "ctrl+f"
 Set-PsFzfOption -PSReadlineChordReverseHistory "ctrl+r"
+
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -EditMode vi
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineOption -PredictionSource History
@@ -21,15 +23,10 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -ViModeIndicator Cursor
 # F2: enter screen capture/selection mode (scroll + select + Enter to copy)
 Set-PSReadLineKeyHandler -Key F2 -Function CaptureScreen
-
 # Vi-mode friendly copy/paste on Space+y / Space+p in command mode
 Set-PSReadLineKeyHandler -Key 'y' -Function Copy  -ViMode Command
 Set-PSReadLineKeyHandler -Key 'p' -Function Paste -ViMode Command
 
-# Vi-mode search style
-# Set-PSReadLineKeyHandler -Key '/' -Function ViSearchHistoryBackward
-# Set-PSReadLineKeyHandler -Key '?' -Function ViSearchHistoryForward
-# Set-PSReadLineOption -EditMode Windows
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
 # Be aware that if you are missing these lines from your profile, tab completion
@@ -69,14 +66,17 @@ function lta3 { eza -lTag --level=3 --icons $args }
 
 Invoke-Expression (&starship init powershell)
 # integrate with wezterm because I use starship
+$prompt = ""
 function Invoke-Starship-PreCommand {
     $current_location = $executionContext.SessionState.Path.CurrentLocation
     if ($current_location.Provider.Name -eq "FileSystem") {
         $ansi_escape = [char]27
         $provider_path = $current_location.ProviderPath -replace "\\", "/"
-        $host.ui.Write("$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\")
+        $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
     }
+    $host.ui.Write($prompt)
 }
+
 # =============================================================================
 #
 # Utility functions for zoxide.
@@ -137,7 +137,7 @@ function global:__zoxide_hook {
 }
 
 # Initialize hook.
-$global:__zoxide_hooked = (Get-Variable __zoxide_hooked -ErrorAction SilentlyContinue -ValueOnly)
+$global:__zoxide_hooked = (Get-Variable __zoxide_hooked -ErrorAction Ignore -ValueOnly)
 if ($global:__zoxide_hooked -ne 1) {
     $global:__zoxide_hooked = 1
     $global:__zoxide_prompt_old = $function:prompt
@@ -150,10 +150,55 @@ if ($global:__zoxide_hooked -ne 1) {
     }
 }
 
+# =============================================================================
+#
+# When using zoxide with --no-cmd, alias these internal functions as desired.
+#
+
+# Jump to a directory using only keywords.
+function global:__zoxide_z {
+    if ($args.Length -eq 0) {
+        __zoxide_cd ~ $true
+    }
+    elseif ($args.Length -eq 1 -and ($args[0] -eq '-' -or $args[0] -eq '+')) {
+        __zoxide_cd $args[0] $false
+    }
+    elseif ($args.Length -eq 1 -and (Test-Path -PathType Container -LiteralPath $args[0])) {
+        __zoxide_cd $args[0] $true
+    }
+    elseif ($args.Length -eq 1 -and (Test-Path -PathType Container -Path $args[0] )) {
+        __zoxide_cd $args[0] $false
+    }
+    else {
+        $result = __zoxide_pwd
+        if ($null -ne $result) {
+            $result = __zoxide_bin query --exclude $result "--" @args
+        }
+        else {
+            $result = __zoxide_bin query "--" @args
+        }
+        if ($LASTEXITCODE -eq 0) {
+            __zoxide_cd $result $true
+        }
+    }
+}
+
+# Jump to a directory using interactive search.
+function global:__zoxide_zi {
+    $result = __zoxide_bin query -i "--" @args
+    if ($LASTEXITCODE -eq 0) {
+        __zoxide_cd $result $true
+    }
+}
+
+# =============================================================================
+#
+# Commands for zoxide. Disable these using --no-cmd.
+#
+
 Set-Alias -Name z -Value __zoxide_z -Option AllScope -Scope Global -Force
 Set-Alias -Name zi -Value __zoxide_zi -Option AllScope -Scope Global -Force
-function cd { z $args }
 
 # Alias for zoxide
-Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
